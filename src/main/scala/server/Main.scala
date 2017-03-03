6251ff7c411158f2a03d40a78439049d385e9cc5
pwdd <pwdd@users.noreply.github.com>
2017-03-03 06:51:19 -0600

    Identify V0.2 runner

--- src/main/scala/server/Main.scala
+++ src/main/scala/server/Main.scala
@@ -4,7 +4,7 @@ import server.connection.Server
 
 object Main {
   def main(args: Array[String]): Unit =  {
-    println("\nRunning v0.1\n")
+    println("\nRunning v0.2\n")
     Server().run()
   }
 }
