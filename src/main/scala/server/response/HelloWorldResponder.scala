3ca8b3825e3e26ef76dc72cf39d1b3c1e65ff3ec
pwdd <pwdd@users.noreply.github.com>
2017-03-09 16:16:50 -0600

    Remove caching info from header of HelloWorldResponder

--- src/main/scala/server/response/HelloWorldResponder.scala
+++ src/main/scala/server/response/HelloWorldResponder.scala
@@ -12,9 +12,6 @@ object HelloWorldResponder extends Responder {
     new ByteArrayInputStream(
       ("HTTP/1.1 200 OK" + CRLF +
       "Date: " + formattedDate + CRLF +
-      "ETag: abcdefg" + CRLF +
-      "Cache-Control: max-age=120" + CRLF +
-      "Last-Modified: Tue, 24 Jan 2012 00:09:09 GMT" + CRLF +
       "Content-type: text/plain" + CRLF +
       "Content-Length: " + text.size + CRLF + CRLF).getBytes)
   }
